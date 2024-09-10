63379603c0984e8925128613f72d230da1bfc662
maxime-louis14 <maximelouis321@gmail.com>
2024-09-10 16:57:41 +0200

    Model de QRcode Struct

--- internal/api/handlers/qr_handler.go
+++ internal/api/handlers/qr_handler.go
@@ -1,23 +1,5 @@
-// qr_handler.go
-
 package handlers
 
 import (
-	"QRFactory/pkg/qr"
+	
 )
-
-// QRHandler encapsule les dépendances nécessaires pour les opérations QR
-type QRHandler struct {
-	qrGenerator *qr.Generator // Instance du générateur QR
-	// Ajoutez d'autres dépendances ici au besoin
-}
-
-// NewQRHandler initialise et retourne une nouvelle instance de QRHandler
-func NewQRHandler(qrGenerator *qr.Generator) *QRHandler {
-	return &QRHandler{
-		qrGenerator: qrGenerator,
-		// Initialisez d'autres dépendances ici au besoin
-	}
-}
-
-// Implementez ici les méthodes de manipulation des QR codes, comme GenerateQRCodeAlphanumeric, GenerateQRCodeNumeric, etc.
--- internal/api/model/qr_code.go
+++ internal/api/model/qr_code.go
@@ -1,1 +1,9 @@
 package model
+
+
+type QRCode struct {
+    Size  int    // Taille du QR code
+    Data  string // Données à encoder
+    Image []byte // Image générée du QR code
+}
+
